d1b095e68304d9ff6005ea710860cd3265012840
rafedraper <rafedraper@gmail.com>
2020-01-24 20:11:36 +0100

    create user interface

--- caesar_cypher.rb
+++ caesar_cypher.rb
@@ -1,8 +1,24 @@
-# frozen_string_literal: true
-
 require 'sinatra'
 require 'sinatra/reloader' if development?
 
 get '/' do
-  'Hello world'
+  string = params['string_input'].to_s
+  number = params['integer_input'].to_i
+  ui_response = caesar_cipher(string, number)
+  erb :index, locals: { ui_response: ui_response }
+end
+
+def caesar_cipher(string, number)
+  r_string = ''
+  string.split('').each do |e|
+    character_code = e.ord
+    r_string << if character_code.between?(65, 90)
+                  (65 + (((character_code - 65) + number) % 26)).chr
+                elsif character_code.between?(97, 122)
+                  (97 + (((character_code - 97) + number) % 26)).chr
+                else
+                  e
+                end
+  end
+  r_string
 end
